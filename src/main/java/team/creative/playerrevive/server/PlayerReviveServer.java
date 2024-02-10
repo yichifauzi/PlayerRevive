8ef55688ffa8c14cd755886139369f2540ec5f84
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-02-10 10:02:51 +0100

    Fixed swimming pose being forced on server even after player has been revived or killed

--- src/main/java/team/creative/playerrevive/server/PlayerReviveServer.java
+++ src/main/java/team/creative/playerrevive/server/PlayerReviveServer.java
@@ -72,6 +72,8 @@ public class PlayerReviveServer {
         NeoForge.EVENT_BUS.post(new PlayerRevivedEvent(player, revive));
         
         sendUpdatePacket(player);
+        
+        player.setForcedPose(null);
     }
     
     public static void kill(Player player) {
@@ -86,6 +88,7 @@ public class PlayerReviveServer {
         player.die(source);
         resetPlayer(player, revive);
         revive.revive(); // Done for compatibility reason for rare scenarios the player will not die
+        player.setForcedPose(null);
         
         PlayerRevive.CONFIG.sounds.death.play(player, SoundSource.PLAYERS);
         
